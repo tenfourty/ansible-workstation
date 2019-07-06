b883219a6fbbd03b3f89b659712b6a170a4c7bd6
Jeremy Brown <jeremy@tenfourty.com>
2019-07-06 17:44:18 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# How to run
+
+```shell
+wget -qO- https://raw.github.com/tenfourty/ansible-workstation/master/bootstrap.sh | bash
+
+```
+
+
---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# you can run this using
+# wget -qO- https://raw.github.com/tenfourty/ansible-workstation/master/bootstrap.sh | bash
+
+# exit if a command fails
+set -e
+
+echo "Installing ansible and git"
+
+dnf install -y ansible
+
+ansible-pull -U https://github.com/tenfourty/ansible-workstation.git
